7d572bc2a8f99899301a797b073abae5a9f1d7f2
[reosakaguchi] <[reo0215@yahoo.ne.jp]>
2022-02-19 07:08:45 +0000

    fix relationships & follows and follwers views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
   end
   
   def edit
@@ -24,13 +22,13 @@ class UsersController < ApplicationController
   end
   
   def follows
-    user = User.find(params[:id])
-    @users = user.following_user
+    @user = User.find(params[:id])
+    @users = @user.following_user.reverse_order
   end
   
   def followers
-    user = User.find(params[:id])
-    @users = user.follower_user
+    @user = User.find(params[:id])
+    @users = @user.follower_user.reverse_order
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
   
   # ユーザーをフォローする
   def follow(user_id)
