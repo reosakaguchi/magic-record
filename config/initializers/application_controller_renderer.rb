dd2c5580d3db035e4bdf6dc343df4dec1b418b13
[reosakaguchi] <[reo0215@yahoo.ne.jp]>
2022-02-24 03:54:38 +0000

    fix /views/users/edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b3d8f69e89b3b097d0a9d9f2d96ac3041cdb3d9dee470db1628238e6d4f0619deca03c9cc8a071a49ef2e0504bc52d645755fd11d996b90c7567a90316f1abbe'
